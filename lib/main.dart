7483f10b1d7e0d863c03b43f2b644b5f0efc4099
Hydervali Yalamkur <hydervaliyalamkur@Hydervalis-MBP.wifi.gsu.edu>
2024-10-02 11:23:31 -0400

    Added button functionality

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,79 @@ class CalculatorApp extends StatelessWidget {
   }
 }
 
-class CalculatorHome extends StatelessWidget {
+class CalculatorHome extends StatefulWidget {
+  @override
+  _CalculatorHomeState createState() => _CalculatorHomeState();
+}
+
+class _CalculatorHomeState extends State<CalculatorHome> {
+  String display = '';
+
+  void buttonPressed(String buttonText) {
+    setState(() {
+      display += buttonText;
+    });
+  }
+
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: Colors.blueAccent,
+          padding: EdgeInsets.symmetric(vertical: 20.0),
+        ),
+        child: Text(
+          buttonText,
+          style: TextStyle(fontSize: 20.0, color: Colors.white),
+        ),
+        onPressed: () => buttonPressed(buttonText),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Calculator')),
-      body: Center(child: Text('Basic UI Ready')),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20.0),
+            alignment: Alignment.centerRight,
+            child: Text(
+              display,
+              style: TextStyle(fontSize: 40.0),
+            ),
+          ),
+          Row(
+            children: [
+              buildButton('1'),
+              buildButton('2'),
+              buildButton('3'),
+            ],
+          ),
+          Row(
+            children: [
+              buildButton('4'),
+              buildButton('5'),
+              buildButton('6'),
+            ],
+          ),
+          Row(
+            children: [
+              buildButton('7'),
+              buildButton('8'),
+              buildButton('9'),
+            ],
+          ),
+          Row(
+            children: [
+              buildButton('0'),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
+
